f7010d0f45fe2fa0577c3e0ef9b41652c6edb80f
tokar <tokar@VDDEVTOKAR40>
2017-01-12 16:22:05 +0100

    Chapter 22 End

--- HelperMethods/HelperMethods/Models/Person.cs
+++ HelperMethods/HelperMethods/Models/Person.cs
@@ -1,18 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
 
 namespace HelperMethods.Models
 {
+    [DisplayName("New Person")]
     public class Person
     {
+        [HiddenInput(DisplayValue = false)]
         public int PersonId { get; set; }
+
+        [Display(Name = "First")]
         public string FirstName { get; set; }
+
+        [Display(Name = "Last")]
         public string LastName { get; set; }
+
+        [Display(Name = "Birth Date")]
+        [DataType(DataType.Date)]
         public DateTime BirthDate { get; set; }
+
         public Address HomeAddress { get; set; }
+
+        [Display(Name = "Approved")]
         public bool IsApproved { get; set; }
+
+        [UIHint("Enum")]
         public Role Role { get; set; }
     }
 
